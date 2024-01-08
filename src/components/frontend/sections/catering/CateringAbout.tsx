d98438cf3d3a58949f789b39c4035f32ea7af0c5
BlakeFernandes <blakef148@gmail.com>
2024-01-08 19:19:28 +1300

    Fix typo in CateringAbout description

--- src/components/frontend/sections/catering/CateringAbout.tsx
+++ src/components/frontend/sections/catering/CateringAbout.tsx
@@ -8,7 +8,7 @@ const CateringAbout = () => {
             title="Delicious Moments, Beautifully Served"
             subtitle="Catering For Aotearoa"
             description={[
-                "Having catered for over 60+ events for exceptionally satisfied customers. We are proud to serve Aotearoa's with our authentic Goan/Indian curries. We have a wide range of catering options available for you to choose from.",
+                "Having catered for over 60+ events for exceptionally satisfied customers. We are proud to serve Aotearoa with our authentic Goan/Indian curries. We have a wide range of catering options available for you to choose from.",
                 ""
             ]}
             images={[]}
