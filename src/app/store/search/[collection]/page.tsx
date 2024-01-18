daabd5e3b2b6ff35fab8a2501cd985f6b2949d7d
BlakeFernandes <blakef148@gmail.com>
2024-01-18 18:18:34 +1300

    Update runtime to 'edge' and comment out font loading in OpengraphImage component

--- src/app/store/search/[collection]/page.tsx
+++ src/app/store/search/[collection]/page.tsx
@@ -6,7 +6,7 @@ import Grid from '~/components/store/grid';
 import ProductGridItems from '~/components/store/layout/product-grid-items';
 import { defaultSort, sorting } from '~/lib/constants';
 
-// export const runtime = 'edge';
+export const runtime = 'edge';
 
 export async function generateMetadata({
   params
